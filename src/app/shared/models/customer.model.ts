b7cbdf7d366ee840ff71375a7b3176d990695198
MMygind <56871472+MMygind@users.noreply.github.com>
2021-12-02 13:31:52 +0100

    Plumbing + Hul igennem til transactions

---
+++ src/app/shared/models/customer.model.ts
@@ -0,0 +1,12 @@
+import {Subscription} from './subscription.model';
+
+export interface Customer {
+  id?: number;
+  name: string;
+  email: string;
+  creationDate: Date;
+  phoneNumber: string;
+  subscription: Subscription;
+  licensePlates: string[];
+  active: boolean;
+}
