4e9ea7103cb2d4b07087c6aeea833944ae9b6699
fauxtistic <56873702+fauxtistic@users.noreply.github.com>
2021-12-10 13:16:37 +0100

    Scroll to top button in transactions view. Now to get it to hide when already at top...

--- src/app/transaction/transaction.page.ts
+++ src/app/transaction/transaction.page.ts
@@ -1,15 +1,19 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {TransactionData} from '../shared/models/transaction.model';
 import {TransactionService} from '../shared/services/transaction.service';
 import {map} from 'rxjs/operators';
 import {DatePipe} from '@angular/common';
+import {IonContent} from "@ionic/angular";
+
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'transaction.page.html',
   styleUrls: ['transaction.page.scss']
 })
-export class TransactionPage implements OnInit{
+export class TransactionPage implements OnInit {
+
+  @ViewChild(IonContent) content: IonContent;
 
   dataSource: TransactionData | undefined;
   newItems: TransactionData | undefined;
@@ -25,13 +29,16 @@ export class TransactionPage implements OnInit{
     this.transactionService.getAllTransactions(1, 10).pipe(map((transactionData: TransactionData) => this.dataSource = transactionData)).subscribe();
   }
 
+  public scrollToTop() {
+    this.content.scrollToTop();
+  }
+
   public loadData(event) {
     this.page ++;
     const maximumPages = this.dataSource.meta.totalPages;
 
     const items = this.dataSource.items;
     this.transactionService.getAllTransactions(this.page, 10).pipe(map((transactionData: TransactionData) => this.newItems = transactionData)).subscribe();
-
 
     if (this.page === maximumPages) {
       event.target.disabled = true;
