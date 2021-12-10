3d3fe40440a0bceeefd93a9f1deda76b15f30747
fauxtistic <56873702+fauxtistic@users.noreply.github.com>
2021-12-10 14:31:50 +0100

    Slider functional, can select location and washtype. Visually completely unsatisfactory however, needs a lot of work.

--- src/app/wash/wash.page.ts
+++ src/app/wash/wash.page.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {LocationService} from '../shared/services/location.service';
 import {LocationModel} from '../shared/models/location.model';
+import {WashType} from '../shared/models/washtype.model';
+import {WashTypeService} from '../shared/services/washtype.service';
+import {IonSlides} from '@ionic/angular';
 
 @Component({
   selector: 'app-wash',
@@ -9,13 +12,39 @@ import {LocationModel} from '../shared/models/location.model';
 })
 export class WashPage implements OnInit {
 
-  locations: LocationModel[];
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400,
+    allowTouchMove: false,
+  };
 
-  constructor(private locationService: LocationService) {}
+  locations: LocationModel[];
+  selectedLocation: LocationModel;
+  washTypes: WashType[];
+  selectedWashType: WashType;
+
+  constructor(private locationService: LocationService,
+              private washTypeService: WashTypeService) {
+  }
 
   ngOnInit(): void {
     this.locationService.getCompanyLocations()
       .subscribe((locations) => this.locations = locations);
   }
 
+  public selectLocation(location: LocationModel, slides: IonSlides, index: number) {
+    this.selectedLocation = location;
+    this.washTypeService.getLocationWashTypes(this.selectedLocation.id)
+      .subscribe((washTypes) => this.washTypes = washTypes);
+    this.gotoSlide(slides, index);
+  }
+
+  public selectWashType(washType: WashType, slides: IonSlides, index: number) {
+    this.selectedWashType = washType;
+    this.gotoSlide(slides, index);
+  }
+
+  public gotoSlide(slides: IonSlides, index: number) {
+    slides.slideTo(index);
+  }
 }
