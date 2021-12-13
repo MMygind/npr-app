8305a7c3cf90921f159fafb609fae8051befccdc
MMygind <56871472+MMygind@users.noreply.github.com>
2021-12-13 16:46:04 +0100

    licenseplate list

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {CustomerService} from '../shared/services/customer.service';
 import {Customer} from '../shared/models/customer.model';
+import {LicensePlate} from '../shared/models/licenseplate.model';
 
 @Component({
   selector: 'app-tab3',
@@ -10,11 +11,17 @@ import {Customer} from '../shared/models/customer.model';
 export class ProfilePage implements OnInit {
 
   loggedInUser: Customer | undefined;
+  plateList: LicensePlate[];
 
   constructor(private customerService: CustomerService) {}
 
   public getCustomerData(): void {
-    this.customerService.getCustomerById(1).subscribe(customer => this.loggedInUser = customer);
+    this.plateList = [];
+    this.customerService.getCustomerById(1).subscribe(customer => {
+      this.loggedInUser = customer;
+      this.plateList = JSON.parse(JSON.stringify(customer.licensePlates));
+      console.log(JSON.stringify(customer.licensePlates));
+    });
   }
 
   ngOnInit(): void {
